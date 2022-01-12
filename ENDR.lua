8b57a5115dec8fb0a18ee53019c43d3e026d9699
robloxprolol <96886392+robloxprolol@users.noreply.github.com>
2022-01-12 14:02:36 +0200

    Update ENDR.lua

--- ENDR.lua
+++ ENDR.lua
@@ -1,3 +1,115 @@
+local main = Instance.new("ScreenGui")
+local epic = Instance.new("Frame")
+local backgroundtitle = Instance.new("TextLabel")
+local creator = Instance.new("TextLabel")
+local title = Instance.new("TextLabel")
+local close = Instance.new("TextButton")
+local ebutton = Instance.new("TextButton")
+local qbutton = Instance.new("TextButton")
+local tbutton = Instance.new("TextButton")
+local main2 = Instance.new("ScreenGui")
+local epic2 = Instance.new("Frame")
+local backgroundtitle2 = Instance.new("TextLabel")
+local creator2 = Instance.new("TextLabel")
+local title2 = Instance.new("TextLabel")
+local close2 = Instance.new("TextButton")
+local fbutton2 = Instance.new("TextButton")
+local ebutton2 = Instance.new("TextButton")
+local qbutton2 = Instance.new("TextButton")
+local tbutton2 = Instance.new("TextButton")
+
+main.Name = "main"
+main.Parent = game.CoreGui
+main.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+epic.Name = "epic"
+epic.Parent = main
+epic.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+epic.Position = UDim2.new(0.0911376476, 0, 0.466830462, 0)
+epic.Size = UDim2.new(0, 181, 0, 178)
+epic.Active = true
+epic.Draggable = true
+
+creator.Name = "creator"
+creator.Parent = epic
+creator.BackgroundColor3 = Color3.fromRGB(1, 1, 1)
+creator.Position = UDim2.new(0.00442049652, 0, 0.762519121, 0)
+creator.Size = UDim2.new(0, 181, 0, 42)
+creator.Font = Enum.Font.SourceSans
+creator.Text = "made by scripty sir"
+creator.TextColor3 = Color3.fromRGB(0, 0, 0)
+creator.TextScaled = true
+creator.TextSize = 14.000
+creator.TextWrapped = false
+
+title.Name = "title"
+title.Parent = epic
+title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+title.BackgroundTransparency = 1.000
+title.Position = UDim2.new(0.0497237556, 0, 0, 0)
+title.Size = UDim2.new(0, 119, 0, 43)
+title.Font = Enum.Font.SciFi
+title.Text = "control key"
+title.TextColor3 = Color3.fromRGB(0, 0, 0)
+title.TextScaled = true
+title.TextSize = 14.000
+title.TextWrapped = true
+
+close.Name = "close"
+close.Parent = epic
+close.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+close.Position = UDim2.new(0.76795578, 0, 0, 0)
+close.Size = UDim2.new(0, 43, 0, 43)
+close.Font = Enum.Font.GothamBlack
+close.Text = "X"
+close.TextColor3 = Color3.fromRGB(0, 0, 0)
+close.TextScaled = true
+close.TextSize = 14.000
+close.TextWrapped = true
+close.MouseButton1Down:connect(function()
+epic.Visible = false
+end)
+
+ebutton.Name = "ebutton"
+ebutton.Parent = epic
+ebutton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ebutton.Position = UDim2.new(0.553093923, 0, 0.654781578, 0)
+ebutton.Size = UDim2.new(0, 84, 0, 42)
+ebutton.Font = Enum.Font.SourceSans
+ebutton.Text = "t"
+ebutton.TextColor3 = Color3.fromRGB(0, 0, 0)
+ebutton.TextSize = 14.000
+ebutton.MouseButton1Down:connect(function()
+   chatfunc("HM, HM, HAHAHAHAHAHA", BrickColor.random().Color)
+			CreateSound("300208779", hed, 10, 1)
+end)
+
+qbutton.Name = "qbutton"
+qbutton.Parent = epic
+qbutton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+qbutton.Position = UDim2.new(0.154781578, 0, 0.053093923, 0)
+qbutton.Size = UDim2.new(0, 84, 0, 42)
+qbutton.Font = Enum.Font.SourceSans
+qbutton.Text = "Burn in hell"
+qbutton.TextColor3 = Color3.fromRGB(0, 0, 0)
+qbutton.TextSize = 14.000
+qbutton.MouseButton1Down:connect(function()
+   BURN_IN_HELL()
+end)
+
+tbutton.Name = "tbutton"
+tbutton.Parent = epic
+tbutton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+tbutton.Position = UDim2.new(0.854781578, 0, 0.963093923, 0)
+tbutton.Size = UDim2.new(0, 84, 0, 42)
+tbutton.Font = Enum.Font.SourceSans
+tbutton.Text = "Destruction"
+tbutton.TextColor3 = Color3.fromRGB(0, 0, 0)
+tbutton.TextSize = 14.000
+tbutton.MouseButton1Down:connect(function()
+   Destruction()
+end)
+
 -----------------------
 --[[ Organization : Insanity ]]--
 --[[ Name : Ender (Thou who collects the souls of innocents) ]]--
